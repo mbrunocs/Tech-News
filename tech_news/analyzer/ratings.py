7443d9d94c3b72caf542a7e4dcc1a3727fba11fc
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-10-30 16:47:57 -0300

    dev: def to top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,12 @@
-# Requisito 10
+from tech_news.database import find_news
+
+
+# ref: https://stackoverflow.com/questions/1143671/
+#       how-to-sort-objects-by-multiple-keys
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = [row for row in find_news() if row["comments_count"] > 0]
+    return [(row["title"], row["url"]) for row in sorted(
+        result, key=lambda k: (-k['comments_count'], k['title']))][:5]
 
 
 # Requisito 11
