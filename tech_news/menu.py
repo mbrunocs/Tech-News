05f8ae1c176f763dabbce39bdf9566c06fbc8bc2
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-10-31 15:59:56 -0300

    dev: analyzer_menu v1.2

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,12 +1,12 @@
 import sys
-from datetime import date
-import tech_news.analyzer.ratings as ratings
-import tech_news.analyzer.search_engine as search
-import tech_news.scraper as scraper
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+from tech_news.analyzer.search_engine import (
+    search_by_title, search_by_date, search_by_tag, search_by_category)
+from tech_news.scraper import get_tech_news
 
 
 def show_menu():
-    input(
+    return input(
         """Selecione uma das opções a seguir:
  0 - Popular o banco com notícias;
  1 - Buscar notícias por título;
@@ -20,47 +20,62 @@ def show_menu():
 
 
 def num_news():
-    command = input("Digite quantas notícias serão buscadas:")
-    scraper.get_tech_news(command)
+    command = int(input("Digite quantas notícias serão buscadas:"))
+    return get_tech_news(command)
 
 
-def search_news():
+def src_news():
     command = input("Digite o título:")
-    search.search_by_title(command)
+    return search_by_title(command)
 
 
 def src_by_date():
     command = input("Digite a data no formato aaaa-mm-dd:")
-    search.search_by_date(date(command))
+    return search_by_date(command)
 
 
 def src_by_tag():
     command = input("Digite a tag:")
-    search.search_by_tag(command)
+    return search_by_tag(command)
 
 
 def src_by_cat():
     command = input("Digite a categoria:")
-    search.search_by_category(command)
+    return search_by_category(command)
+
+
+def shutdown():
+    return print("Encerrando script\n")
+
+
+def get_5_news():
+    return top_5_news()
+
+
+def get_5_cat():
+    return top_5_categories()
+
+
+def verify_input(string, command):
+    if string not in command or string == "":
+        raise KeyError
+    return string
 
 
 def analyzer_menu():
-    show_menu()
-    _, path, args = sys.argv
     try:
         command = dict({
-            '0': search_news,
-            '1': num_news,
+            '0': num_news,
+            '1': src_news,
             '2': src_by_date,
             '3': src_by_tag,
             '4': src_by_cat,
-            '5': ratings.top_5_news,
-            '6': ratings.top_5_categories,
-            '7': sys.stdout.write("Encerrando script\n"),
+            '5': get_5_news,
+            '6': get_5_cat,
+            '7': shutdown,
         })
-        if path not in command or path == "":
-            raise KeyError
-        command[path]
+        opt = verify_input(show_menu(), command)
+        return command[opt]()
     except KeyError:
         sys.stderr.write("Opção inválida\n")
 # path do sys.argv, controle somente do comando tech-news-analyzer?
